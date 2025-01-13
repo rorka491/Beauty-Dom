0b07435086ce76981ce208ada0371cf4bdc93cd7
rodion <rodion.gorshkov.05@mail.ru>
2025-01-14 02:45:32 +0700

    Новый коммит

--- Beauty_Dom/mainapp/tests.py
+++ Beauty_Dom/mainapp/tests.py
@@ -1,11 +1,7 @@
 from django.test import TestCase
 from datetime import timedelta, datetime, date, time
 
-today = date.today()
-start_date = datetime.today()
 
-print(type(today), type(start_date))
-print(today, 'ggg' ,start_date)
 
 
 # Create your tests here.
